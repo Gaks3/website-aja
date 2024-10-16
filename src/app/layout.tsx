497b3aeaf73c3c97e2a7a2d8641df927295bf9b9
Gaks3 <dwi66116@gmail.com>
2024-10-17 02:04:07 +0700

    feat: add google site verification metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,17 @@
+import { type Metadata } from 'next'
 import './globals.css'
 
 type Props = {
   children: React.ReactNode
+}
+
+export const metadata: Metadata = {
+  title: 'WebGue | Build Modern with WebGue',
+  description:
+    'Create stunning, high-performance websites effortlessly with WebsiteAja.',
+  verification: {
+    google: '_-X36VnEIWAWaq6cTM8oxBtvycZTDqRUnIvbaAuCdB4',
+  },
 }
 
 export default function RootLayout({ children }: Props) {
