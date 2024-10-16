40c4065a9efa1c3883b554f4b40687974a608c11
Gaks3 <dwi66116@gmail.com>
2024-10-17 02:14:38 +0700

    fix: not found page error

---
+++ src/app/[locale]/not-found.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+
+const NotFound = () => {
+  return (
+    <html lang='us'>
+      <body>
+        <div className='flex h-screen w-full flex-col items-center justify-center gap-3'>
+          <Image src={'/circle-logo.png'} alt='Logo' width={100} height={100} />
+          <div className='flex items-center space-x-3 divide-x'>
+            <p className='text-xl font-bold'>404</p>
+            <h1 className='pl-3'>Not Found Page</h1>
+          </div>
+        </div>
+      </body>
+    </html>
+  )
+}
+
+export default NotFound
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 const NotFound = () => {
   return (
     <div className='flex h-screen w-full flex-col items-center justify-center gap-3'>
-      <Image src={'/logo.png'} alt='Logo' width={100} height={100} />
+      <Image src={'/circle-logo.png'} alt='Logo' width={100} height={100} />
       <div className='flex items-center space-x-3 divide-x'>
         <p className='text-xl font-bold'>404</p>
         <h1 className='pl-3'>Not Found Page</h1>
