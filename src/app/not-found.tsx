70900b90e957cab4e097c981a39642b6e017c816
Gaks3 <dwi66116@gmail.com>
2024-10-17 01:47:52 +0700

    feat: add handle not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+
+const NotFound = () => {
+  return (
+    <div className='flex h-screen w-full flex-col items-center justify-center gap-3'>
+      <Image src={'/logo.png'} alt='Logo' width={100} height={100} />
+      <div className='flex items-center space-x-3 divide-x'>
+        <p className='text-xl font-bold'>404</p>
+        <h1 className='pl-3'>Not Found Page</h1>
+      </div>
+    </div>
+  )
+}
+
+export default NotFound
