e1155b1cea48f38b75bee3f3b9cd52e20f849b4d
Gaks3 <dwi66116@gmail.com>
2024-10-17 08:04:21 +0700

    fix: remove google verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,9 +9,6 @@ export const metadata: Metadata = {
   title: 'WebGue | Build Modern with WebGue',
   description:
     'Create stunning, high-performance websites effortlessly with WebsiteAja.',
-  verification: {
-    google: '_-X36VnEIWAWaq6cTM8oxBtvycZTDqRUnIvbaAuCdB4',
-  },
 }
 
 export default function RootLayout({ children }: Props) {
